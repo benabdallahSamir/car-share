c8923da97a3df12f159b3807efb4a0bd60a395e8
samirbenabdallah <samirbenabdallah4002@gmail.com>
2025-05-19 21:49:11 +0100

    change data place

--- utils/multer.js
+++ utils/multer.js
@@ -4,7 +4,7 @@ import path from 'path';
 // Configure storage
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads/') // Make sure this directory exists
+        cb(null, 'public/') // Make sure this directory exists
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname)
