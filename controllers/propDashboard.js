35f0f28b990a157fa50a857581bb166d7d939685
samirbenabdallah <samirbenabdallah4002@gmail.com>
2025-05-20 09:48:03 +0100

    add the rest of neccesair routes in prop dashboard

--- controllers/propDashboard.js
+++ controllers/propDashboard.js
@@ -63,3 +63,89 @@ export async function gotoPropDisponibility(req, res) {
     res.render("500");
   }
 }
+
+export async function getoPropReservations(req, res) {
+  const { user } = req;
+  try {
+    const reservations = [];
+    res.render("prop dashboard/reservations", {
+      user,
+      reservations,
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("500");
+  }
+}
+export async function getoPropRevenu(req, res) {
+  const { user, userId } = req;
+  try {
+    const statistique = {
+      totalAnnuel: 120000,
+      annuelMoyenne: 10000,
+      annuelMoyennePercent: {
+        isHigher: true,
+        number: 0,
+      },
+      annulation: 0,
+      annulationPercent: {
+        isHigher: true,
+        number: 0,
+      },
+      reservationTotalPercent: {
+        isHigher: true,
+        number: 0,
+      },
+    };
+    let myCars = await Car.find({ userId });
+    myCars = myCars.map((car) => {
+      const reservationTotalPrix = 182030;
+      const reservationPercent = 100;
+      car.reservationTotalPrix = reservationTotalPrix;
+      car.reservationPercent = reservationPercent;
+      return car;
+    });
+    res.render("prop dashboard/revenus", {
+      user,
+      myCars,
+      statistique,
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("500");
+  }
+}
+
+export async function getoPropChat(req, res) {
+  const { user, isLogged } = req;
+  try {
+    res.render("prop dashboard/chat", {
+      user,
+      isLogged,
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("500");
+  }
+}
+
+export async function getoPropMyCars(req, res) {
+  const { user, userId } = req;
+  try {
+    let myCars = await Car.find({ userId, est_enable: true });
+    myCars = myCars.map((car) => {
+      const reservations = 0;
+      const total = 0;
+      car.reservations = reservations;
+      car.total = total;
+      return car;
+    });
+    res.render("prop dashboard/mes-vehicules", {
+      user,
+      myCars,
+    });
+  } catch (error) {
+    console.log(error);
+    res.render("500");
+  }
+}
--- routes/propdashboard.js
+++ routes/propdashboard.js
@@ -1,10 +1,20 @@
 import express from "express";
 import { verifyToken } from "../middlewars/token.js";
-import { getoMainPropDashboard, gotoPropDisponibility } from "../controllers/propDashboard.js";
+import {
+  getoMainPropDashboard,
+  getoPropChat,
+  getoPropMyCars,
+  getoPropReservations,
+  getoPropRevenu,
+  gotoPropDisponibility,
+} from "../controllers/propDashboard.js";
 const router = express.Router();
 
 router.get("/dashboard-proprietaire", verifyToken, getoMainPropDashboard);
 router.get("/disponibilite", verifyToken, gotoPropDisponibility);
-
+router.get("/reservations", verifyToken, getoPropReservations);
+router.get("/revenus", verifyToken, getoPropRevenu);
+router.get("/mes-vehicules", verifyToken, getoPropMyCars);
+router.get("/chat", verifyToken, getoPropChat);
 
 export default router;
